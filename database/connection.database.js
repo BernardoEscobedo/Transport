569f19c237d80252777c429299f4fd5d08d0f19d
BernardoEscobedo <berna.lsc20@gmail.com>
2026-03-03 21:26:05 -0600

    cambios

---
+++ database/connection.database.js
@@ -0,0 +1,27 @@
+import 'dotenv/config'
+import pg from 'pg'
+
+const { Pool, Client } = pg
+
+const db = new Pool({
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    ssl: {
+        rejectUnauthorized: false, // IMPORTANTE para Render
+    },
+    allowExitOnIdle: true
+});
+
+//verificar la conexion
+try{
+    await db.query('SELECT NOW()')
+    console.log('✅ Conexión exitosa a PostgreSQL')
+}catch(error){
+    console.error('❌ Error en la conexión a la BD:', error)
+}
+
+//const client = new Client ();
+export { db };

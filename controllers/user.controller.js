f1fa14226b66736b6307e3b41fd70258eec92632
BernardoEscobedo <berna.lsc20@gmail.com>
2026-03-18 21:59:45 -0600

    user models 6

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -48,3 +48,50 @@ const registrarUsuario = async (req,res) =>{
         })
     }
 }
+
+const login = async(req,res)=>{
+    try{
+        const {correo, password_hash} = req.body
+
+        const missingFields = []
+        if(!correo) missingFields.push('correo')
+        if(!password_hash) missingFields.push('password_hash')
+        
+        if(missingFields.length>0){
+            return res.status(400).json({
+                ok:false,
+                msg: `Faltan los siguientes campos: ${missingFields.join(', ')}`
+            });
+        }
+        const usuario = await userModel.encontrarPorCorreo(correo)
+        if(!usuario){
+            return res.status(404).json({ok:false, msg:"Correo no encontrado"})
+        }
+
+        const isMatch=await bcrypt.compare(password_hash, usuario.password_hash)
+
+        if(!isMatch){
+            return res.status(401).json({ok: false, msg:"Contraseña incorrecta"})
+        }
+
+        const token = jwt.sign({correo: usuario.correo, id_tipo_usuario: usuario.id_tipo_usuario},
+            process.env.JWT_SECRET,
+            {
+                expiresIn:"2h"
+            }
+        )
+
+        return res.json({
+            ok:true,
+            token,
+            id_tipo_usuario: usuario.id_tipo_usuario,
+            correo: correo.correo
+        })
+    }catch(error){
+        console.log(error)
+        return res.status(500).json({
+            ok:false,
+            msg: 'Error en el servidor'
+        })
+    }
+}
